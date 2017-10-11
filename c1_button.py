ee1dbcb107e54a1e5dd9d3bcf23bbb0621a7e54a
pcuinty <ciusubv.g6@dispostable.com>
2017-10-11 23:59:09 +0200

    updated copyright and description in code

--- c1_button.py
+++ c1_button.py
@@ -1,10 +1,24 @@
-# -*- coding: utf-8 -*-
+# -*- mode: python ; coding: utf-8 -*-
+# original copyright 2013 Thomas TEMPE <thomas.tempe@alysse.org>
+# as Strikethrough button in editor window
+# and original copyright 2013 Roland Sieker <ospalh@gmail.com>
+# as Spanify Text
+#
+# License: GNU AGPL, version 3 or later;
+# http://www.gnu.org/licenses/agpl.html
+
+u"""
+Anki2 add-on to put text in c1 cloze tag.
+This is an Anki2 add-on that adds a button to the card
+editor. Clicking on the button wraps the selected text in
+{{c1::}}.
+"""
 
 from anki.hooks import addHook
 from aqt.editor import Editor
 
 def add_c1_brackets(editor):
-    u"""Call js function to wrap text in the span."""
+    u"""Call js function to wrap text in c1 tag."""
     editor.web.eval(ur"""wrap('\{\{c1::', '\}\}');""")
 
 
